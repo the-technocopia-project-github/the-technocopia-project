be21da6e7a0888d698ab5c06b4b8f6a14efddada
Hai Pham <hpham@LTwist.(none)>
2013-04-19 19:33:30 -0400

    added current files from main git host

---
+++ applications/TechnocopiaKitFileGenerator/src/com/technocopia/kit/KitFileGeneratorMain.java
@@ -0,0 +1,12 @@
+package com.technocopia.kit;
+
+public class KitFileGeneratorMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Starting Kit FIle Generator");
+	}
+
+}
